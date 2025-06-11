3bf99886e47eb7f7ce33e8ff86d0c98818989300
Riley Paul <rileypaul96@gmail.com>
2025-06-10 21:39:24 -0600

    add user to sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -7,8 +7,15 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar";
+import AppSidebarUser from "./app-sidebar-user";
+import { useQuery } from "@tanstack/react-query";
+import { qCurrentUser } from "@/lib/client/queries";
 
 const AppSidebar: React.FC = () => {
+  const { data: user } = useQuery(qCurrentUser);
+
+  if (!user) return null;
+
   return (
     <Sidebar>
       <SidebarHeader>Excalidraw</SidebarHeader>
@@ -16,7 +23,9 @@ const AppSidebar: React.FC = () => {
         <SidebarGroup />
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <AppSidebarUser user={user} />
+      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
