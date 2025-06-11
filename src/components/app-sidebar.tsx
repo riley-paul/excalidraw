7936c029fedaf77de279e901541be0f9b31aa726
Riley Paul <rileypaul96@gmail.com>
2025-06-10 18:22:23 -0600

    add all of shadcn

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+const AppSidebar: React.FC = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -2,12 +2,18 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 
 import React from "react";
 import "@excalidraw/excalidraw/index.css";
+import { SidebarProvider, SidebarTrigger } from "./ui/sidebar";
+import AppSidebar from "./app-sidebar";
 
 const Canvas: React.FC = () => {
   return (
-    <div className="h-screen">
-      <Excalidraw />
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="h-screen w-full relative">
+        <SidebarTrigger className="absolute top-16 left-4 z-10" />
+        <Excalidraw />
+      </div>
+    </SidebarProvider>
   );
 };
 
