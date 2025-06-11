7bad93f695edee991f3ee88dbeb60ddc903bd3cc
Riley Paul <rileypaul96@gmail.com>
2025-06-11 07:04:11 -0600

    cleanup nav drawings

--- src/components/nav/nav-drawings.tsx
+++ src/components/nav/nav-drawings.tsx
@@ -19,58 +19,75 @@ import {
   ArrowUpRightIcon,
   LinkIcon,
   MoreHorizontalIcon,
+  PlusIcon,
   StarOffIcon,
   Trash2Icon,
 } from "lucide-react";
 import { useIsMobile } from "@/hooks/use-mobile";
+import useMutations from "@/hooks/use-mutations";
+
+const NavDrawingMenu: React.FC<{ drawing: MinimalDrawingSelect }> = ({
+  drawing,
+}) => {
+  const isMobile = useIsMobile();
+  const { removeDrawing } = useMutations();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <SidebarMenuAction showOnHover>
+          <MoreHorizontalIcon />
+          <span className="sr-only">More</span>
+        </SidebarMenuAction>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-56 rounded-lg"
+        side={isMobile ? "bottom" : "right"}
+        align={isMobile ? "end" : "start"}
+      >
+        <DropdownMenuItem>
+          <StarOffIcon className="text-muted-foreground" />
+          <span>Remove from Favorites</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LinkIcon className="text-muted-foreground" />
+          <span>Copy Link</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <ArrowUpRightIcon className="text-muted-foreground" />
+          <span>Open in New Tab</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => removeDrawing.mutate(drawing)}>
+          <Trash2Icon className="text-muted-foreground" />
+          <span>Delete</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 type Props = {
   drawings: MinimalDrawingSelect[];
 };
 
 const NavDrawings: React.FC<Props> = ({ drawings }) => {
-  const isMobile = useIsMobile();
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarGroupLabel>Drawings</SidebarGroupLabel>
       <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton variant="outline">
+            <PlusIcon />
+            <span>Create new drawing</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
         {drawings.map((item) => (
           <SidebarMenuItem key={item.title}>
             <SidebarMenuButton asChild>
               <span>{item.title}</span>
+              <NavDrawingMenu drawing={item} />
             </SidebarMenuButton>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuAction showOnHover>
-                  <MoreHorizontalIcon />
-                  <span className="sr-only">More</span>
-                </SidebarMenuAction>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="w-56 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align={isMobile ? "end" : "start"}
-              >
-                <DropdownMenuItem>
-                  <StarOffIcon className="text-muted-foreground" />
-                  <span>Remove from Favorites</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <LinkIcon className="text-muted-foreground" />
-                  <span>Copy Link</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <ArrowUpRightIcon className="text-muted-foreground" />
-                  <span>Open in New Tab</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <Trash2Icon className="text-muted-foreground" />
-                  <span>Delete</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
           </SidebarMenuItem>
         ))}
       </SidebarMenu>
